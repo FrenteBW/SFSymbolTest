f5530351dae1d2f511e0d46637c642aefdb9daa2
An Byungwook <abw2619@icloud.com>
2023-09-19 15:32:13 +0900

    Initial Commit

---
+++ SFSymbolTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SFSymbolTest
+//
+//  Created by 안병욱의 mac on 2023/09/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SFSymbolTest/SFSymbolTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFSymbolTestApp.swift
+//  SFSymbolTest
+//
+//  Created by 안병욱의 mac on 2023/09/19.
+//
+
+import SwiftUI
+
+@main
+struct SFSymbolTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
